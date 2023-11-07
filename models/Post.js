a72b31b1cfefac4048f9a00dc8f067222cdaac2b
MAT-2 <mpcasey30@gmail.com>
2023-11-07 15:45:52 -0600

    Added models for the Post and User js files to create models.

--- models/Post.js
+++ models/Post.js
@@ -3,11 +3,21 @@ const sequelize = require("../config/connection");
 
 class Post extends Model {}
 
+//JS representation of creating our table for the POSTS that will be created.
+
 Post.int({
   id: {
     type: DataTypes.INTEGER,
     allowNull: false,
     primaryKey: true,
     autoIncrement: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  body: {
+    type: DataTypes.STRING,
+    allowNull: false,
   },
 });

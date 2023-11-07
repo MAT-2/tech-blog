09fb239221c6ff3c7b10ffabf41a2eead358369f
MAT-2 <mpcasey30@gmail.com>
2023-11-06 22:54:23 -0600

    Added intializations for express, handlebars, and Sequelize.

--- server.js
+++ server.js
@@ -5,3 +5,13 @@ const routes = require("controllers");
 const session = require("express-session");
 const helpers = require("./utils/helpers");
 const sequelize = require("./config/connection");
+
+//Initializing express.js
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//Handlebar.jS initialization
+const hbs = exhbs.create({ helpers });
+
+//Initializing Sequelize
+const SequelizeStore = require("connect-session-sequelize")(session.Store);

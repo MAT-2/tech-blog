9d7afe03a9d6d085b6d6e7b76932d113a28afd61
MAT-2 <mpcasey30@gmail.com>
2023-11-07 15:53:12 -0600

    Added hooks to hash new user data, passwords, and the updated user data. Added configurations for user model.

--- models/User.js
+++ models/User.js
@@ -7,23 +7,49 @@ class User extends Model {
   }
 }
 
-User.init({
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-});
+  {
+    hooks: {
+      //Before the password is created, the password is hashed and scrambled so it would not be readable; doubles security.
+      //10 refers to as salt rounds, how many times the password will be hashed to be complete.
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 8);
+        return updatedUserData;
+      },
+      //Does it again when it updates.
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.password = await bcrypt.hash(
+          updatedUserData.password,
+          8
+        );
+        return updatedUserData;
+      },
+    },
+    //Configs for model. ORM used, timestamps if something is created in database, freeze table prevents pluralization, allows underscores to fill spaces, and name of model that the data is being stored (table name).
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "user",
+  }
+);

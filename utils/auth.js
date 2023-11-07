a12c0bb882a336d1d6ec3e2e5511bbd89bfb4d6c
MAT-2 <mpcasey30@gmail.com>
2023-11-07 16:10:39 -0600

    Added get HTTP route to render the homepage.handlebars. Added middleware in auth.js in case a user is not logged into the website.

---
+++ utils/auth.js
@@ -0,0 +1,11 @@
+//Middleware if a user is not logged into the website.
+
+const withAuth = (req, res, next) => {
+  if (!req.session.logged_in) {
+    res.redirect("/login");
+  } else {
+    next();
+  }
+};
+
+module.exports = withAuth;

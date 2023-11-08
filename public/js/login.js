c706d6eb1c2304e15536a04323f1c1a91ab65869
MAT-2 <mpcasey30@gmail.com>
2023-11-07 23:38:30 -0600

    Added if statement functionality to check if a user login and signup. Added userData.js in seeds file to add example data to see if functionality works properly.

--- public/js/login.js
+++ public/js/login.js
@@ -5,5 +5,36 @@ const loginForm = async (event) => {
   const password = document.querySelector("#password").value.trim();
 
   if (email && password) {
+    const response = await fetch("/api/users/login", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (response.ok) {
+      document.location.replace("/dashboard");
+    } else {
+      alert(err);
+    }
+  }
+};
+
+const signupForm = async (event) => {
+  event.preventDefault();
+
+  const name = document.querySelector("#name").value.trim();
+  const email = document.querySelector("emailNew").value.trim();
+  const password = document.querySelector("passwordNew").value.trim();
+
+  if (name && email && password) {
+    const response = await fetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({ name, email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (response.ok) {
+      document.location.replace("/dashboard");
+    } else {
+      alert(err);
+    }
   }
 };

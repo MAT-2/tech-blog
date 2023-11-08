3453a241f4055a12b44270073bf8ed29096d0cf1
MAT-2 <mpcasey30@gmail.com>
2023-11-07 23:30:06 -0600

    Added script src to login.handlebars, connected routes for index.js under api folder for controllers, and created login.js and added two variable to query select email and password IDs for login form.

---
+++ public/js/login.js
@@ -0,0 +1,9 @@
+const loginForm = async (event) => {
+  event.preventDefault();
+
+  const email = document.querySelector("#email").value.trim();
+  const password = document.querySelector("#password").value.trim();
+
+  if (email && password) {
+  }
+};
